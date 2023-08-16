b276bde5dea037e0e4d864a46f543d0fa022383a
Lucas C / llucbrell <llucbrell@tutanota.com>
2023-08-16 12:02:52 +0200

    fixed bug

--- micdn.js
+++ micdn.js
@@ -1,4 +1,7 @@
-window.onload =  function cambiarFondo(){
+function cambiarFondo(){
     console.log("Esto es una prueba de javascript");
+}
 
+window.onload = function(){
+    // Aquí puedes poner cualquier código que quieras que se ejecute cuando la página se haya cargado completamente
 }
